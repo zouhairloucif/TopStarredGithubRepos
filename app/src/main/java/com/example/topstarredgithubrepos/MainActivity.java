8deb6cd81cbd4da55b84703135928fbb8ae1fee8
Zouhair Loucif <zouhair.loucif@gmail.com>
2019-04-15 15:23:44 +0100

    Raw Application initiated with the function to check the internet connection availability

---
+++ app/src/main/java/com/example/topstarredgithubrepos/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.topstarredgithubrepos;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+
+//        Check if Internet Connection is Available
+        if(!isNetworkAvailable()){
+            Toast.makeText(MainActivity.this, "No Internet Connection!",Toast.LENGTH_LONG).show();
+            return;
+        }
+    }
+}
